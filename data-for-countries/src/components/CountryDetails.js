2e7644fbed46fe909651b4c2466179146e71c47a
Llewellyn Dsouza <lledsouza2209@gmail.com>
2021-03-21 20:49:51 +0530

    Button added

---
+++ data-for-countries/src/components/CountryDetails.js
@@ -0,0 +1,18 @@
+const CountryDetails = ({country}) => {
+  return (
+    <div>
+      <h1>{country.name}</h1>
+      <p>capital {country.capital}</p>
+      <p>population {country.population}</p>
+      <h3>laguages</h3>
+      <ul>
+        {country.languages.map((language) => (
+          <li key={language.iso639_1}>{language.name}</li>
+        ))}
+      </ul>
+      <img src={country.flag} alt="flag" />
+    </div>
+  );
+};
+
+export default CountryDetails;
