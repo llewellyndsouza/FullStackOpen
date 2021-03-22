a78e85e038371e4fd8a4942509362fd234a944a7
Llewellyn Dsouza <lledsouza2209@gmail.com>
2021-03-22 19:01:43 +0530

    Logging and Middleware

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,14 @@ const app = express();
 
 app.use(express.json());
 
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
 let notes = [
   {
     id: 1,
@@ -75,6 +83,16 @@ app.post("/api/notes", (request, response) => {
   response.json(note);
 });
 
+// For any unknown URLs
+app.use(requestLogger);
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+// Listening to port
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
---
+++ phonebook-server/README.md
@@ -0,0 +1,4 @@
+# Phonebook Server
+
+npm run dev
+- starts the server and watches for changes in index.js file
