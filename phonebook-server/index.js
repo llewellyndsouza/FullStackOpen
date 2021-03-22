42a3ed2c443e4453ea6dee7e1b475274d0ab4c5d
Llewellyn Dsouza <lledsouza2209@gmail.com>
2021-03-22 18:48:38 +0530

    PhonebookServer step6 handle errors and duplicates

--- phonebook-server/index.js
+++ phonebook-server/index.js
@@ -2,31 +2,37 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   {
     name: "Arto1 Hellas1",
     number: "040-87476657",
-    id: 1,
+    id: 14524,
   },
   {
     name: "Arto2 Hellas2",
     number: "040-652456",
-    id: 2,
+    id: 28657836,
   },
   {
     name: "Arto3 Hellas3",
     number: "040-123456",
-    id: 3,
+    id: 3757867,
   },
   {
     name: "Arto4 Hellas4",
     number: "040-12748234",
-    id: 4,
+    id: 475527,
   },
 ];
 
 const totalContacts = () => {
   return persons.length;
+};
+
+const generateId = () => {
+  return (Math.floor(Math.random()*1000000000000))
 };
 
 app.get("/info", (request, response) => {
@@ -48,27 +54,63 @@ app.get("/api/persons/:id", (request, response) => {
   const person = persons.find((p) => p.id === id);
   console.log("person:", person);
   if (person) {
-    response.json(person)
+    response.json(person);
   } else {
     response.status(404).end();
   }
 });
 
-app.delete("/api/persons/:id",(request,response)=>{
+const checkDuplicate = (newName) => {
+  return persons.find(person=>person.name===newName);
+}
+
+app.post("/api/persons", (request, response) => {
+  console.log("Add new person");
+  const body = request.body;
+  console.log(body);
+
+  console.log("Checking data integrity");
+  if (!body.name || !body.number) {
+    console.log("Content missing");
+    return response.status(400).json({
+      error: "Content missing",
+    });
+  }
+
+  console.log("Checking for duplicates");
+  if(checkDuplicate(body.name)) {
+    console.log("Duplicate found");
+    return response.status(400).json({
+      error: "Name already exists"
+    })
+  }
+  
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+
+  persons = persons.concat(person);
+  console.log("New person added");
+  response.json(person);
+});
+
+app.delete("/api/persons/:id", (request, response) => {
   console.log("Request to delete");
   const id = Number(request.params.id);
   console.log("id:", id);
   const person = persons.find((p) => p.id === id);
   console.log("person:", person);
   if (person) {
-    persons = persons.filter(person=>person.id!==id)  
+    persons = persons.filter((person) => person.id !== id);
     console.log("deleted person");
     response.status(204).end();
   } else {
-    console.log('person not found');
+    console.log("person not found");
     response.status(404).end();
   }
-})
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
